86479084bb0d9a793a8301146d392661b40938c5
leiy-zhangt <1372872918@qq.com>
2022-10-15 11:05:03 +0800

    default

--- System/sys/sys.c
+++ System/sys/sys.c
@@ -1,38 +1,38 @@
 #include "sys.h"  
 //////////////////////////////////////////////////////////////////////////////////	 
 
-//ϵͳʱ�ӳ�ʼ��	
-//����ʱ������/�жϹ���/GPIO���õ�
+//系统时钟初始化	
+//包括时钟设置/中断管理/GPIO设置等
 
 //////////////////////////////////////////////////////////////////////////////////  
 
 
-//THUMBָ�֧�ֻ������
-//�������·���ʵ��ִ�л��ָ��WFI  
+//THUMB指令不支持汇编内联
+//采用如下方法实现执行汇编指令WFI  
 __asm void WFI_SET(void)
 {
 	WFI;		  
 }
-//�ر������ж�(���ǲ�����fault��NMI�ж�)
+//关闭所有中断(但是不包括fault和NMI中断)
 __asm void INTX_DISABLE(void)
 {
 	CPSID   I
 	BX      LR	  
 }
-//���������ж�
+//开启所有中断
 __asm void INTX_ENABLE(void)
 {
 	CPSIE   I
 	BX      LR  
 }
-//����ջ����ַ
-//addr:ջ����ַ
+//设置栈顶地址
+//addr:栈顶地址
 __asm void MSR_MSP(u32 addr) 
 {
 	MSR MSP, r0 			//set Main Stack value
 	BX r14
 }
-//�����жϷ���
+//设置中断分组
 void NVIC_Configuration(void)  
 { 
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);	
@@ -42,35 +42,25 @@ void RCC_Configuration(void)
 {
     ErrorStatus HSEStartUpStatus;
     RCC_DeInit();
-    RCC_HSEConfig(RCC_HSE_ON); /* Enable HSE ʹ���ⲿ���پ���*/   
-	HSEStartUpStatus = RCC_WaitForHSEStartUp(); /* Wait till HSE is ready �ȴ��ⲿ���پ���ʹ�����*/   
+    RCC_HSEConfig(RCC_HSE_ON); /* Enable HSE 使能外部高速晶振*/   
+	HSEStartUpStatus = RCC_WaitForHSEStartUp(); /* Wait till HSE is ready 等待外部高速晶振使能完成*/   
 	if(HSEStartUpStatus == SUCCESS){   
-		/*����PLLʱ��Դ����Ƶϵ��*/   
-		RCC_PLLConfig(RCC_PLLSource_HSE,8,336,2,7); //����PLLʱ��Ƶ�� 
-		/*����AHBʱ�ӣ�HCLK��*/   
-		RCC_HCLKConfig(RCC_SYSCLK_Div1); //RCC_SYSCLK_Div1����AHBʱ�� = ϵͳʱ��(SYSCLK) = 168MHZ���ⲿ����8HMZ��   
-		/*ע��˴������ã����ʹ��SYSTICK����ʱ���򣬴�ʱSYSTICK(Cortex System timer)=HCLK/8=9MHZ*/   
-		RCC_PCLK1Config(RCC_HCLK_Div4); //���õ���AHBʱ�ӣ�PCLK1��,RCC_HCLK_Div4����APB1ʱ�� = HCLK/4 = 42MHZ���ⲿ����8HMZ��   
-		RCC_PCLK2Config(RCC_HCLK_Div2); //���ø���AHBʱ�ӣ�PCLK2��,RCC_HCLK_Div1����APB2ʱ�� = HCLK = 84MHZ���ⲿ����8HMZ��   
-		/*ע��AHB��Ҫ�����ⲿ�洢��ʱ�ӡ�APB2����AD��I/O���߼�TIM������1��APB1����DA��USB��SPI��I2C��CAN������2��3��4��5����ͨTIM */  
-		FLASH_SetLatency(FLASH_Latency_5); //����FLASH�洢����ʱʱ��������     
-		FLASH_PrefetchBufferCmd(ENABLE); //ѡ��FLASHԤȡָ�����ģʽ��Ԥȡָ����ʹ�� 
-        //FLASH�ӳٲ�����Ҫ�ο���ѹ��ϵͳƵ��
-		RCC_PLLCmd(ENABLE);	//ʹ��PLL
-		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET); //�ȴ�PLL����ȶ�   
-		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); //ѡ��SYSCLKʱ��ԴΪPLL
-		while(RCC_GetSYSCLKSource() != 0x08); //�ȴ�PLL��ΪSYSCLKʱ��Դ 
+		/*设置PLL时钟源及倍频系数*/   
+		RCC_PLLConfig(RCC_PLLSource_HSE,8,336,2,7); //设置PLL时钟频率 
+		/*设置AHB时钟（HCLK）*/   
+		RCC_HCLKConfig(RCC_SYSCLK_Div1); //RCC_SYSCLK_Div1——AHB时钟 = 系统时钟(SYSCLK) = 168MHZ（外部晶振8HMZ）   
+		/*注意此处的设置，如果使用SYSTICK做延时程序，此时SYSTICK(Cortex System timer)=HCLK/8=9MHZ*/   
+		RCC_PCLK1Config(RCC_HCLK_Div4); //设置低速AHB时钟（PCLK1）,RCC_HCLK_Div4——APB1时钟 = HCLK/4 = 42MHZ（外部晶振8HMZ）   
+		RCC_PCLK2Config(RCC_HCLK_Div2); //设置高速AHB时钟（PCLK2）,RCC_HCLK_Div1——APB2时钟 = HCLK = 84MHZ（外部晶振8HMZ）   
+		/*注：AHB主要负责外部存储器时钟。APB2负责AD，I/O，高级TIM，串口1。APB1负责DA，USB，SPI，I2C，CAN，串口2，3，4，5，普通TIM */  
+		FLASH_SetLatency(FLASH_Latency_5); //设置FLASH存储器延时时钟周期数     
+		FLASH_PrefetchBufferCmd(ENABLE); //选择FLASH预取指缓存的模式，预取指缓存使能 
+        //FLASH延迟参数需要参考电压与系统频率
+		RCC_PLLCmd(ENABLE);	//使能PLL
+		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET); //等待PLL输出稳定   
+		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK); //选择SYSCLK时钟源为PLL
+		while(RCC_GetSYSCLKSource() != 0x08); //等待PLL成为SYSCLK时钟源 
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 
